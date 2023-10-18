770a7674f36f3c7fc8e23dc994a413c43ff1a4e1
Simon Prophet <simon.s.prophet@outlook.com>
2023-10-18 06:12:46 +0100

    add JS to page nav with Smooth Scrolling to sections

--- script.js
+++ script.js
@@ -53,3 +53,12 @@ btnLearnMore.addEventListener('click', (e) => {
 });
 
 // Page navigation
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  //Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
